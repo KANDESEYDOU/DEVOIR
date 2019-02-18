804a14fdbb05d4b7c0fa73ba92b22d22c687f2c9
kande <saidouyoussouphk@gmail.com>
2019-02-18 23:43:17 +0000

    Ajout de fichier

---
+++ Information.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FICHE D'INFORMATIONS</title>
+	<meta charset="utf-8">
+</head>
+<style type="text/css">
+	form{
+		background-color: white;
+        width: 50%;
+	}
+    label{
+    	text-align: center;
+    	text-align: none;
+    }
+    legend{
+    	background-color: aqua;
+    }
+    footer{
+    	text-align: center;
+    	background-color: aqua;
+    }
+    body{
+    	background-color: gray;
+    	background-image: url(hacking.jpg);
+    	background-repeat: no-repeat;
+		background-size: 105%;
+    }
+
+    img{
+    	font-size: 100%;
+     }
+
+</style>
+<body>
+
+	<div align="center">
+	<form method="POST" action="recueil.php">
+		<legend><br> VOS COORDONEES PERSONNELLES <br><br></legend>
+		<fieldset>
+		<table>
+		<img src="téléchargé.png" width="15%" height="10%"><br><br><br>
+
+		<label for="Prénom"><small>Prénom:</small></label>
+		<input type="text" name="Prénom" placeholder="Votre Prénom"><br><br><br>
+
+		<label for="Nom"><small> Nom:</small></label>
+		<input type="text" name="Nom" placeholder="Votre Nom"><br><br><br>
+
+		<label for="Email"><small> Email: </small></label>
+        <input type=Email" id="Email" name="Email" placeholder="kande@gmail.com"><br><br><br>
+
+        <label for="password"><small> Mot de passe: </small></label>
+        <input type="password" name="mpd1" placeholder="Mot de passe"><br><br><br>
+
+        <label for="password"><small>Confirmer: </small></label>
+        <input type="password" name="mpd2" placeholder="Confirmer passe"><br><br><br>
+
+		<label for="Téléphone"> <small>Téléphone: </small></label>
+		<input type="text" name="Téléphone" placeholder=" 77 667 49 07"><br><br><br>
+
+		<label for="Date"> <small>Date de naissance: </small></label>
+		<input type="text" name="Date" placeholder=" 18/12/1997"><br><br><br>
+
+		<label for="ids"> <small>Identifiant Slack: </small></label>
+		<input type="text" name="ids" placeholder=" nom@slack.com "><br><br><br>
+
+		<input type="submit" name="Valider" align="rigth">
+        </table>
+	</form></fieldset></div>
+	<footer width: 100% ><hr> 
+	<p>Pour plus d'informations contactez-nous sur</p>
+	<p>saidouyoussouphk@gmail.com    ou  le 77 667 49 07 </p><br><hr>
+	</footer>
+</body>
+</html>
